99a777a89f59977976574b5ddb21cdd57649c45a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-03 18:37:59 -0600

    Added more features, fixed answer_check.rb

--- lib/answer_check.rb
+++ lib/answer_check.rb
@@ -1,12 +1,16 @@
 module AnswerCheck
 
   def check_answer(answer, input)
+    clone = answer.dup
     correct_elements = 0
     correct_indicies = 0
-    answer.each_with_index do |letter, index|
+    clone.each_with_index do |letter, index|
       if input.include?(letter)
         correct_elements += 1
+        letter = nil
       end
+    end
+    answer.each_with_index do |letter, index|
       if letter == input[index]
         correct_indicies += 1
       end
--- lib/printer.rb
+++ lib/printer.rb
@@ -2,23 +2,25 @@ module Printer
 
   def welcome_txt
     puts "\e[H\e[2J"
-    puts "Hello there! Welcome to Mastermind!\n"
+    puts "Hello there! Welcome to Mastermind!"
     puts "Press (p) to play, (i) for instructions or (q) to quit."
-    puts "You can also cheat by pressing (c), but come one, you're better than that."
+    puts "You can also cheat by pressing (c), but come on, you're better than that."
   end
 
   def instructions_txt
     puts "\e[H\e[2J"
-    puts "I will generate a sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow."
+    puts "I will generate a sequence with four" \
+    "elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow."
     puts "Your task is that you must guess(type) the correct combination of colors"
-    puts "(a color may also appear more than once - it's even possible that a sequence contains the same color up to four times!)."
+    puts "(a color may also appear more than once -
+    it's even possible that a sequence contains the same color up to four times!)."
   end
 
   def play_txt
     puts "\e[H\e[2J"
     puts "I have generated a sequence with four elements made up of:"
     puts "(r)ed, (g)reen, (b)lue, and (y)ellow."
-    puts "Use (q)uit at any time to end the game.What's your guess?"
+    puts "Use (q)uit at any time to end the game. What's your guess?"
   end
 
   def quit_txt
@@ -31,9 +33,21 @@ module Printer
     puts "Invalid guess. Answers must be four letters long."
   end
 
-  def hint_txt
+  def cheat_txt
     puts "\e[H\e[2J"
-    
+    puts "Grand-scale cheating: the answer is #{@answer.join}"
+  end
+
+  def wrong_txt
+    puts "Nope, but you have #{@number1}
+    correct colors, #{@number2} correct indicies."
+    puts "and you've taken #{@guess_number} guess#{"es" if @guess_number > 1}."
+  end
+
+  def win_txt
+    puts "\e[H\e[2J"
+    puts "Congratulations! You've won absolutely nothing!\nBut you did guess the correct sequence of #{@answer.join} in #{@mins} minute#{'s' if @mins != 1} and #{@secs} seconds."
+    puts "Good job or something. Do you want to (p)lay again or (q)uit?"
   end
 
 end
